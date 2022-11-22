928bffed5f089c44aad86cebca9e25d9a0ae8a75
Dima <dmitriy.matveev89@inbox.ru>
2022-11-22 10:29:48 +0300

    + implementation ver.2.0

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 import java.util.Scanner;
 
 public class Main {
+    public static final int MC_LENGTH_10 = 10;
+    public static final int MC_LENGTH_11 = 11;
+    public static final char MC_BEG_SYMB_7 = '7';
+    public static final char MC_BEG_SYMB_8 = '8';
+    public static final char MC_BEG_STR_7 = '7';
+
     public static void main(String[] args) {
 //***
 //        Реализуйте  удаление лишних символов при вводе номера телефона в консоли и
@@ -15,12 +21,35 @@ public class Main {
         String loPhone = loScanner.nextLine();
         loPhone = loPhone.replaceAll("[^0-9]", "");
 
-//        Проверка на количество символов
-//        Если 11 - проверяем первый символ, должен быть либо 7 либо 8(меняем на 7), иначе ошибка "Неверный формат номера"
-//        Если меньше 10 - "Неверный формат номера"
-//        Если 10, добавляем 7
-//        Если больше 10 - "Неверный формат номера"
+        StringBuffer loSb = new StringBuffer(loPhone);
 
+        //Проверка на валидность
+        if (loPhone.length() == MC_LENGTH_11) {
+            //Если 11 - проверяем первый символ, должен быть либо 7 либо 8(меняем на 7),
+            //иначе ошибка "Неверный формат номера"
+            //Избегать оператора switch
+            switch (loPhone.charAt(0)) {
+                case MC_BEG_SYMB_7:
+                    break;
+                case MC_BEG_SYMB_8:
+                    //(меняем на 7 первый символ)
+                    loSb.setCharAt(0, MC_BEG_SYMB_7);
+                    loPhone = loSb.toString();
+                    break;
+                default:
+                    System.out.println("Uncorrect number");
+                    return;
+            }
+        } else if (loPhone.length() == MC_LENGTH_10) {
+            //Если 10, добавляем 7
+            loSb.insert(0, MC_BEG_STR_7);
+            loPhone = loSb.toString();
+        } else if ((loPhone.length() < MC_LENGTH_10) || (loPhone.length() > MC_LENGTH_11)) {
+            //Если меньше 10 - "Неверный формат номера"
+            //Если больше 11 - "Неверный формат номера"
+            System.out.println("Uncorrect number");
+            return;
+        }
         System.out.println(loPhone);
     }
 }
